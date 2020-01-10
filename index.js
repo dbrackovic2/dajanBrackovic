943629eb1e078bbcc4f3cfb2a3a55ff75cec69ff
master joda <mjoda@localhost.localdomain>
2020-01-10 01:57:28 +0100

    add project

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req,res) => res.send('Dajan Brackovic <3 Belma Masic'));
-
-app.listen(port, () => console.log(`Nice web app listening on port ${port}!`));
